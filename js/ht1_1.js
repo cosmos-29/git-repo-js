e75d58524f40c4b8dfe0619bc3c5a4c8c139fd6e
cosmos-29 <a.a.kraskov@gmail.com>
2019-02-15 18:02:27 +0300

    Попробовал все внести через инпуты

--- js/ht1_1.js
+++ js/ht1_1.js
@@ -1,17 +1,25 @@
 /*Functions*/
-function getFloat(valueTemp){ //объявление функции по замене запятой на точку.
-  return valueTemp.replace(/\,/g,'.');
-  }
-function rounding(roundValue) { //объявленеи функции по округлению чисел.
-	return roundValue.toFixed(2);
+function convert() {
+    var number = parseInt(document.getElementById("userAnswer").value);
+    return number;
+    // return +document.getElementById("userAnswer").value;
 }
+
+function getFloat(valueTemp) { //объявление функции по замене запятой на точку.
+    return valueTemp.replace(/\,/g, '.');
+}
+
+function rounding(roundValue) { //объявленеи функции по округлению чисел.
+    return roundValue.toFixed(2);
+}
+function write(text) {
+        document.getElementById("userAnswer").innerHTML = text;
+    }
 /* Объявление переменных*/
-var temperatureCel = prompt("Введите температуру °C");
-var valueTempCel = + getFloat(temperatureCel);
+var temperatureCel = convert();
+var valueTempCel = +getFloat(temperatureCel);
 var temperatureFar;
-
 /* Solution*/
-temperatureFar = (9/5)*valueTempCel + 32;
-
+temperatureFar = (9 / 5) * valueTempCel + 32;
 /*  Print */
-alert(rounding(temperatureFar) + " °F");
+write(rounding(temperatureFar));

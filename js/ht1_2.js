a33c2c148e1cc4c79205ec2e7154c841145fbb65
cosmos-29 <a.a.kraskov@gmail.com>
2019-02-15 14:18:17 +0300

    Задание выполнено в составе: 1. Задать температуру в градусах по Цельсию. Вывести в  alert соответствующую температуру в градусах по Фаренгейту. Подсказка: расчет идет по формуле  Tf = (9 / 5) * Tc + 32 , где  Tf — температура по Фаренгейту,  Tc  — по Цельсию. 2. Объявить две переменные:  admin и  name . Записать в  name строку "Василий"; Скопировать © geekbrains.ru 7 значение из  name  в  admin . Вывести  admin  (должно вывестись «Василий»). 3. * Чему будет равно JS-выражение 1000 + "108"? 4. * Самостоятельно разобраться с атрибутами тега  script  ( async  и  defer ).

---
+++ js/ht1_2.js
@@ -0,0 +1,4 @@
+var admin;
+var name = prompt("Введите name" );
+admin = name;
+alert("Имя администратора :" + admin)
---
+++ js/ht1_3.js
@@ -0,0 +1,4 @@
+var firstValue = +1000;
+var secondValue = "108";
+var summValue = firstValue+secondValue;
+alert ("результат " + summValue);

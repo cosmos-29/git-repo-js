2aff18904c6157922b2cb4a755ac62a99872030c
cosmos-29 <a.a.kraskov@gmail.com>
2019-02-27 16:45:17 +0300

    Доделал =)

--- homeTask4/shoppingCart.js
+++ homeTask4/shoppingCart.js
@@ -1,47 +1,36 @@
 //записываем вложенные массивы в 0-ой ячейке - наименование товара, в ячейке 1 - стоимость, в ячейке 2 - количество.
-var itemsBasket = [
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-{
-	product: [ 300, 3,],
-},
-];
+var itemsBasket = {
+	product: [1, 300, 3,],
+
+	product1: [1, 300, 3,],
+
+	product2: [1, 300, 3,],
+
+	product3: [1, 300, 3,],
+
+	product4: [1, 300, 3,],
+
+	product5: [1, 300, 3,],
+
+	product6: [1, 300, 3,],
+
+	product7: [1, 300, 3,],
+
+	product8: [1, 300, 3,],
+
+	product9: [1, 300, 3,],
+};
 //Функция по подсчету стоимости корзины
 function arrFromObject(allData) {
-	return allData = Array.from(allData);
+	return allData = Object.values(allData);
 }
 
 function countBasketPrice(x) {
 	var i = 0;
 	var sum = 0;
 	while(i <=(x.length - 1)) {
-		var m = +x.product[i][0];
-		var n = +x.product[i][1];
+		var m = +x[i][1];
+		var n = +x[i][2];
 		var sum = sum + (m * n);
 		i++;
 	}

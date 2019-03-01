4a2d075bb36bbff3faf52225f95a1ccac1fb33c9
cosmos-29 <a.a.kraskov@gmail.com>
2019-03-01 19:02:57 +0300

    Исправлена ошибка

--- homeTask4/digitToObject.js
+++ homeTask4/digitToObject.js
@@ -1,56 +1,17 @@
-var objectFromDigit = {
-	digits: "Единицы",
-	tens: "Десятки",
-	hundreds: "сотни",
-	addDigitToObject: function(inputAllData) {
-			var i =  inputAllData.length - 1;
-			if (i == 0) {
-				this.digits = inputAllData[0];
-				this.tens = '';
-				this.hundreds = '';
-				} else if (i = 1) {
-				this.hundreds = '';
-				this.tens = inputAllData[0];
-				this.digits = inputAllData[1];
-
-				}
-				 else {
-				this.hundreds = inputAllData[0];
-				this.tens = inputAllData[1];
-				this.digits = inputAllData[2];
-				}
-			} 
-
-		}
-	
-
-
-function addDigit() {
-	var p = "";
-	p = p + prompt("Введите число от 1 до 1000");
-	return p;
+function numberToObject(num) { 
+    if (num < 0) {
+        throw new Error ("Число не может быть отрицательным.");
+        return;
+    }
+    var keys = ['units', 'dozens', 'hundreds'];
+    num = num + "";
+    if (num.length > keys.length) {
+        throw new Error ("Число не может быть таким большим!");
+    }
+    var obj = {};
+    for (var i = num.length - 1; i >= 0; i--) {
+        var key = keys[num.length - 1 - i];
+        obj[key] = num[i];
+    }
+    return obj;
 }
-
-function checkDigit() {
-	var t = addDigit()
-	while (t.length > 3) {
-		t = addDigit();
-	}
-	return t;
-}
-
-function inputToArray(anyData) {
-	var arr = []
-	for( var j = 0; j < anyData.length; j++) {
-		arr.push(anyData[j]);		
-	}
-	return arr;
-}
-
-var inputData = checkDigit();
-
-objectFromDigit.addDigitToObject((inputToArray(inputData)));
-
-
-
-

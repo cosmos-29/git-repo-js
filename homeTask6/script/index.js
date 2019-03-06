4143abdf4c26e613bc4bd2f20c389e4b3ef322c0
cosmos-29 <a.a.kraskov@gmail.com>
2019-03-06 16:10:09 +0300

    Создал модальное окно =))

--- homeTask6/script/index.js
+++ homeTask6/script/index.js
@@ -211,6 +211,33 @@ function catalogVisualItem(productItem, indexItem) {
 
 	//Функция init
 
+
+	// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 	function init() {
 	    mesageCart('В корзине пусто.')
 	    var $clear = document.getElementById('clear')
@@ -221,4 +248,5 @@ function catalogVisualItem(productItem, indexItem) {
 	    
 	}
 	window.addEventListener('load', init) 
+
 
